a2362e1118051db3228d4c2e176dc7a5477f9d87
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-04-11 22:54:28 +0700

    ### Setup Splash Screen - Define SplashActivity as a main intent on the MainActivity - Remove action bar from themes.xml

--- beer_cafe_ui/app/src/main/java/com/example/beer_cafe_ui/MainActivity.kt
+++ beer_cafe_ui/app/src/main/java/com/example/beer_cafe_ui/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.beer_cafe_ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.beer_cafe_ui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        signInHandler()
+    }
+
+    private fun signInHandler() {
+        binding?.btnSignin?.setOnClickListener {
+            val intent = Intent(this@MainActivity, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
