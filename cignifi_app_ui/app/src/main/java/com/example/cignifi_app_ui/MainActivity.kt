05edc9067cca08c4ccdd3827d09985f6b077b971
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-04-16 14:11:20 +0700

    ### Styling Main Activity (Sign Up Screen)

--- cignifi_app_ui/app/src/main/java/com/example/cignifi_app_ui/MainActivity.kt
+++ cignifi_app_ui/app/src/main/java/com/example/cignifi_app_ui/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.cignifi_app_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cignifi_app_ui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
     }
 }
