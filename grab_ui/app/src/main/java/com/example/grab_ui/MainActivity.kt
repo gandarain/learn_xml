115b363af157fcf9d61c725ee7d2f66326804d86
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-04-18 21:44:16 +0700

    ### Navigate to Verify OTP - Create a handler for phone number edit text. - Navigate to Verify OTP screen when phone number filled with 10 char.

--- grab_ui/app/src/main/java/com/example/grab_ui/MainActivity.kt
+++ grab_ui/app/src/main/java/com/example/grab_ui/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.grab_ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import com.example.grab_ui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        otpInputTextHandler()
+    }
+
+    private fun otpInputTextHandler() {
+        binding?.etPhoneNumber?.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.length === 10) {
+                    val intent = Intent(this@MainActivity, VerifyOtpActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        })
     }
 }
