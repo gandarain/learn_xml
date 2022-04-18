3defced8b02a3d1b2dd69db2384972f8220018e8
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-04-18 19:54:15 +0700

    ### Create Splash Screen UI

---
+++ grab_ui/app/src/main/java/com/example/grab_ui/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.grab_ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.grab_ui.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private var binding: ActivitySplashScreenBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
